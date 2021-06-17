b7f3053f991a666935784d23e499ce3b21b79916
Ludawkriss <ludvig.grenmo@student.kyh.se>
2021-06-17 11:13:05 +0200

    Updates

---
+++ precache-manifest.f6ba1d131c4c23b408f1f33ceab6e004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a55f1dd7917ef62afd1a0929d2aba24",
+    "url": "/zittron-website/index.html"
+  },
+  {
+    "revision": "5d45da5399db84c59973",
+    "url": "/zittron-website/static/js/2.33ea1e10.chunk.js"
+  },
+  {
+    "revision": "ea11c278d5345c259b089f62e805b8d5",
+    "url": "/zittron-website/static/js/2.33ea1e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06f5940ad6d775b781d",
+    "url": "/zittron-website/static/js/main.029e1352.chunk.js"
+  },
+  {
+    "revision": "6588751b6eb97b10e38e",
+    "url": "/zittron-website/static/js/runtime-main.99e080f4.js"
+  },
+  {
+    "revision": "c67cfa28ceca0c7f8233763ddc48b8f7",
+    "url": "/zittron-website/static/media/heroimg.c67cfa28.png"
+  },
+  {
+    "revision": "4ea2f243ea7ac4c12da61cbbe54aa838",
+    "url": "/zittron-website/static/media/pricing.4ea2f243.png"
+  }
+]);
