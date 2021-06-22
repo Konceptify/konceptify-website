b4019c60851d7482390cfb00ea571ae90062c4db
Ludawkriss <ludvig.grenmo@student.kyh.se>
2021-06-22 09:48:12 +0200

    mm

--- src/emailkey.js
+++ src/emailkey.js
@@ -1,5 +1,5 @@
 export default {
-	USER_ID: `user_wxOBkkzgwByLqSwq3gKCV`, //userID
-	TEMPLATE_ID: `template_o0onus4`, //templateID
-	SERVICE_ID: `service_seqausp`, //serviceID
+	USER_ID: process.env.REACT_APP_USER_ID, //userID
+	TEMPLATE_ID: process.env.REACT_APP_TEMPLATE_ID, //templateID
+	SERVICE_ID: process.env.REACT_APP_SERVICE_ID, //serviceID
 }
