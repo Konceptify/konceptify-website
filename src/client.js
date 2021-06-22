6f63c1d139ffa8d8495885fa0e21b86375bb3bbd
Ludawkriss <ludvig.grenmo@student.kyh.se>
2021-06-22 09:15:57 +0200

    security fixes

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-	projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
-	dataset: process.env.SANITY_STUDIO_API_DATASET,
+	projectId: process.env.REACT_APP_SANITY_STUDIO_API_PROJECT_ID,
+	dataset: process.env.REACT_APP_SANITY_STUDIO_API_DATASET,
 	useCdn: true,
 })
