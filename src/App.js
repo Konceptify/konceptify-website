2e22fd4fa94d78a4223601ff74789611cdc28686
Ludawkriss <ludvig.grenmo@student.kyh.se>
2021-09-06 14:44:20 +0200

    mm

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import { Cookie } from './components/Cookie'
 import { Cookies } from './pages/Cookies'
 /* import useGaTracker from './useGaTracker' */
 import ReactGA from 'react-ga'
-ReactGA.initialize('UA-206812801-1')
+ReactGA.initialize(process.env.REACT_APP_USER_ID)
 
 export const LanguageContext = React.createContext()
 
