d9fa752744bfb3048f3d5dba77fe497bb47ea379
Ludvig Grenmo <ludvig.grenmo@gmail.com>
2022-02-15 14:57:27 +0100

    STHLM tapas added

--- src/utils/clientsData.js
+++ src/utils/clientsData.js
@@ -2,6 +2,7 @@ import pinchos from '../img/pinchos.png'
 import mjukmjuk from '../img/mjukmjuk.png'
 import bores from '../img/bores.png'
 import gps from '../img/gps.png'
+import sthlmTapas from '../img/sthlm-tapas.png'
 
 export const clients = [
 	{ name: 'MjukMjuk', photo: mjukmjuk, link: 'https://mjukmjuk.se/' },
@@ -12,4 +13,9 @@ export const clients = [
 		link: 'https://www.guiltypleasure.se/',
 	},
 	{ name: 'Pinchos', photo: pinchos, link: 'https://www.pinchos.se/' },
+	{
+		name: 'STHLM Tapas',
+		photo: sthlmTapas,
+		link: 'https://www.sthlmtapas.se/',
+	},
 ]
